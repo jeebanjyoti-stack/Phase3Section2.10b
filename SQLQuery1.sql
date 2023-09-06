fdf63ab56689a200685a356db7651de475a3b03f
jeeban <jeebanjyoti193@gmail.com>
2023-09-06 22:40:33 +0530

    first commit

---
+++ SQLQuery1.sql
@@ -0,0 +1,12 @@
+create database School01
+use School01
+
+create table Student(
+Id int primary key not null,
+Name varchar(100) not null,
+Address varchar(100) not null,
+Email varchar(75) not null,
+Class varchar(5) not null)
+
+
+
